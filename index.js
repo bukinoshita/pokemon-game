865f33d62e61a343103ce6a395fffaacc77764e4
Bu Kinoshita <bukinoshita@gmail.com>
2017-06-30 14:06:22 -0400

    pokemon game

---
+++ index.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+'use strict'
+
+const meow = require('meow')
+const updateNotifier = require('update-notifier')
+
+const cli = meow(
+  `
+  Usage:
+    $ pokemon
+
+  Example:
+    $ pokemon
+
+  Options:
+    -h, --help                Show help options
+    -v, --version             Show version
+`,
+  {
+    alias: {
+      h: 'help',
+      v: 'version'
+    }
+  }
+)
+
+updateNotifier({ pkg: cli.pkg }).notify()
+
+const run = () => {
+  const cmd = cli.input[0]
+
+  if (cmd === 'catch') {
+    // 1.0 Get a random pokemon
+    // 2.0 Run probability of pokemon running
+    // 2.1 If pokemon runs -> throw message
+    // 2.2 If not, show bag with pokeballs and items available to use
+    // 3.0 Use item to froze, paralyze etc pokemon
+    // 3.1 Run probability to affect pokemon
+    // 3.2 Choose pokeball
+    // 3.3 Run probability to catch pokemon
+    // 4.0 Caught || Throw pokeball again || pokemon run
+  }
+
+  if (cmd === 'shop') {
+    // 1.0 Fetch user info (needs to be logged in)
+    // 1.1 Welcome user to shop and show balance
+    // 2.0 List of pokeballs and items
+    // 3.0 Buy / Sell (item or pokeball)
+  }
+
+  if (cmd === 'battle') {
+    // Soon
+  }
+
+  if (cmd === 'login') {
+    // Login user and persist (not sure yet)
+  }
+
+  if (cmd === 'signup') {
+    // Signup user, send email to verify, login user and persist (not sure yet)
+  }
+
+  if (cmd === 'whoami') {
+    // Show user stats (balance, name, pokemons, how many pokemons etc)
+  }
+
+  if (cmd === 'rankings') {
+    // Show rankings (who captured more pokemons)
+  }
+}
+
+run()
---
+++ readme.md
@@ -0,0 +1,18 @@
+# pokemon-game [![Build Status](https://travis-ci.org/bukinoshita/pokemon-game.svg?branch=master)](https://travis-ci.org/bukinoshita/pokemon-game)
+
+## In progress
+
+## Related
+
+- [catch-pokemon](https://github.com/bukinoshita/catch-pokemon) — Algorithm to catch a pokemon
+- [pokedex-api](https://github.com/bukinoshita/pokedex-api) — Pokedex API
+- [pokemon-catch-probability](https://github.com/bukinoshita/pokemon-catch-probability) — Check probabilty to catch a pokemon
+- [get-pokeball](https://github.com/bukinoshita/get-pokeball) — Get pokeball information
+- [pokeball-shake](https://github.com/bukinoshita/pokeball-shake) — Algorithm to determine how many times pokeball shakes
+- [pokemon-capture-quote](https://github.com/bukinoshita/pokemon-capture-quote) — Pokemon capture quote
+- [pokemon-f](https://github.com/bukinoshita/pokemon-f) — Algorithm to calculate `f` on capture pokemon method
+
+
+## License
+
+MIT © [Bu Kinoshita](https://bukinoshita.io)
