16763d13ecc89b25a76e1e89f9b58e0c92e3d24c
Bu Kinoshita <bukinoshita@gmail.com>
2017-07-10 17:53:54 -0400

    update user when trying to catch pokemon (https://github.com/bukinoshita/pokemon-game/issues/9)

--- bin/pokemon-catch.js
+++ bin/pokemon-catch.js
@@ -13,38 +13,26 @@ const chalk = require('chalk')
 const getRandomPokemon = require('./../lib/get-random-pokemon')
 const userActions = require('./../lib/user-actions')
 const throwPokeball = require('./../lib/throw-pokeball')
+const { getUser, read, updateUser } = require('./../lib/cfg')
 
 module.exports = async () => {
   const spinner = ora('Finding pokemon...')
   spinner.start()
 
   try {
-    // User fake api
-    const user = {
-      name: 'Bu Kinoshita',
-      bag: [
-        { name: 'Poké ball', slug: 'pokeball', quantity: 10 },
-        { name: 'Ultra ball', slug: 'ultraball', quantity: 5 },
-        { name: 'Great ball', slug: 'greatball', quantity: 7 },
-        { name: 'Master ball', slug: 'masterball', quantity: 1 }
-      ]
-    }
-    // 1.0 Get a random pokemon
+    const user = await getUser(read().token)
     const pokemon = await getRandomPokemon()
+
     spinner.stop()
     shoutMessage(`A wild ${chalk.green.bold(pokemon.name)} appeared!\n`)
 
-    // 2.0 Run probability of pokemon running
     if (pokemonEscape(pokemon.fleeRate)) {
-      // 2.1 If pokemon runs -> throw message
       return shoutMessage(`The Pokemon fled! Don't believe it.`)
     }
 
-    // 3.0 If not, show user actions (bag/run)
     const userAction = await userActions()
 
     if (userAction.answer === 'run') {
-      // 4.0 (3.0 run) Run probability of escaping from wild pokemon
       if (escape(50, pokemon.speed)) {
         return shoutMessage('You escaped')
       }
@@ -52,18 +40,23 @@ module.exports = async () => {
       shoutMessage(`You didn't escape\n`)
     }
 
-    // 4.0 (3.0 bag) Choose pokeball
     const pokeballChosen = await throwPokeball(user.bag)
     const caught = await catchPokemon(pokemon.name, pokeballChosen.answer, {
       hp: pokemon.hp,
       catchRate: pokemon.catchRate
     })
 
-    // 5.0 Run probability to catch pokemon
     if (caught === `All right! ${pokemon.name} was caught!`) {
+      await updateUser(read().token, {
+        type: pokeballChosen.answer,
+        pokemonId: pokemon._id
+      })
       return shoutSuccess(caught)
     }
 
+    await updateUser(read().token, {
+      type: pokeballChosen.answer
+    })
     shoutMessage(caught)
   } catch (err) {
     spinner.stop()
