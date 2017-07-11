b3215fd17a30fe560e0b8a095f7e4b46fd621e8b
Bu Kinoshita <bukinoshita@gmail.com>
2017-07-10 20:00:00 -0400

    whoami data (https://github.com/bukinoshita/pokemon-game/issues/11)

--- bin/pokemon-whoami.js
+++ bin/pokemon-whoami.js
@@ -1,10 +1,23 @@
 'use strict'
 
 const shoutMessage = require('shout-message')
+const shoutError = require('shout-error')
+const chalk = require('chalk')
 
-const { read } = require('./../lib/cfg')
+const { read, getUser } = require('./../lib/cfg')
 
-module.exports = () => {
-  const user = read().user
-  return shoutMessage(user.name || user.email)
+module.exports = async () => {
+  const token = read().token
+  let user
+  try {
+    user = await getUser(token)
+  } catch (err) {
+    return shoutError(`We couldn't fetch your user information.`)
+  }
+
+  /* eslint-disable no-unused-expressions */
+  user.name ? shoutMessage(user.name) : null
+  shoutMessage(`${user.email} ${user.emailConfirmed ? chalk.green('✔') : 'x'}`)
+  shoutMessage(`Balance: $${user.balance}`)
+  shoutMessage(`You have ${user.pokemons.length} pokemons`)
 }
